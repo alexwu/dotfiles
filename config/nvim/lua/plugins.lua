631673e52b523a78636e26d0a601e0c68d32e20e
Alex Wu <adwu@pm.me>
2021-02-20 00:02:08 -0800

    Moves packer compile location

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,38 @@
+local cmd = vim.cmd
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+cmd [[packadd packer.nvim]]
+
+return require('packer').startup({ function()
+  use { 'wbthomason/packer.nvim', opt = true }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+  use {
+    'glepnir/lspsaga.nvim',
+    requires = {'neovim/nvim-lspconfig'}
+  }
+  use { 'Yggdroot/indentLine' }
+  use { 'chaoren/vim-wordmotion' }
+  use { 'connorholyday/vim-snazzy' }
+  use { 'f-person/git-blame.nvim' }
+  use { 'hrsh7th/nvim-compe' }
+  use { 'tpope/vim-abolish' }
+  use { 'tpope/vim-commentary' }
+  use { 'tpope/vim-eunuch' }
+  use { 'tpope/vim-projectionist' }
+  use { 'tpope/vim-surround' }
+  use { 'voldikss/vim-floaterm' }
+end,
+config = {
+        compile_path = fn.stdpath('data')..'packer/packer_compiled.vim',
+        } })
