7ffedd9dc05383a12ca04450764a37557cec10b6
Alex Wu <adwu@pm.me>
2021-03-04 22:01:14 -0800

    Allows toggling of Floaterm while in terminal mode

--- config/nvim/lua/plugins/floaterm.lua
+++ config/nvim/lua/plugins/floaterm.lua
@@ -1,3 +1,4 @@
 vim.g.floaterm_borderchars = "─│─│╭╮╯╰"
 
 vim.api.nvim_set_keymap("n", "<F10>", "<cmd>FloatermToggle<CR>", {noremap = true})
+vim.api.nvim_set_keymap("t", "<F10>", "<cmd>FloatermToggle<CR>", {noremap = true})
