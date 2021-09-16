228d911661793333cc9b2e0486329daebd677553
Alex Wu <adwu@pm.me>
2021-09-16 14:35:56 -0700

    fixes mapping

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -11,7 +11,7 @@ nnoremap { "k", "gk" }
 nnoremap { "<C-j>", "5gj" }
 nnoremap { "<C-k>", "5gk" }
 nnoremap { "<C-h>", "5h" }
-nnoremap { "<C-l>", "5h" }
+nnoremap { "<C-l>", "5l" }
 
 xnoremap { "<C-j>", "5gj" }
 xnoremap { "<C-k>", "5gk" }
