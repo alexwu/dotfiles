acec933f3943298d0ea18e45892c40d39c5cfead
Alex Wu <adwu@pm.me>
2021-09-27 10:55:21 -0700

    Further updates to toggleterm migration

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -12,8 +12,34 @@ local rails_console = Terminal:new {
   },
 }
 
+local rails_runner = Terminal:new {
+  cmd = "bundle exec rails runner " .. vim.fn.expand "%",
+  direction = "float",
+  float_opts = {
+    border = "rounded",
+    width = vim.fn.round(0.9 * vim.o.columns),
+    height = vim.fn.round(0.9 * vim.o.lines),
+    winblend = 0,
+    highlights = { border = "FloatBorder", background = "Normal" },
+  },
+  close_on_exit = false,
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(
+      term.bufnr,
+      "n",
+      "q",
+      "<cmd>close<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+}
+
 function Rails_console()
   rails_console:toggle()
+end
+
+_G.runner = function()
+  rails_runner:toggle()
 end
 
 require("toggleterm").setup {
@@ -47,3 +73,4 @@ vim.cmd [[autocmd FileType toggleterm nmap <buffer> - +]]
 vim.cmd [[autocmd FileType toggleterm nmap <buffer> <space><space> <cmd>ToggleTerm<CR>]]
 vim.cmd [[autocmd FileType toggleterm tmap <buffer> <esc> <C-\><C-n>]]
 vim.cmd [[command! -nargs=0 RConsole :lua Rails_console()<CR>]]
+vim.cmd [[command! -nargs=0 RRunner :lua runner()<CR>]]
