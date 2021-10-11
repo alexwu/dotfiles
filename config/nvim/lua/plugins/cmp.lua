ef9cf80d32f002fc4763f05e342640a8c796d66e
Alex Wu <adwu@pm.me>
2021-10-11 09:48:47 -0700

    Fix breaking changes from nvim-cmp

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -11,10 +11,6 @@ local has_words_before = function()
     and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
-local feedkey = function(key)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
-end
-
 cmp.setup {
   sources = {
     { name = "luasnip" },
@@ -36,8 +32,8 @@ cmp.setup {
     ["<C-SPACE>"] = cmp.mapping.complete(),
     ["<C-E>"] = cmp.mapping.close(),
     ["<TAB>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey "<C-n>"
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
@@ -50,8 +46,8 @@ cmp.setup {
       "s",
     }),
     ["<S-TAB>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey "<C-p>"
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
