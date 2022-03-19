cd9ce363fbc7bd743056141c418f4970bb22ca77
Alex Wu <adwu@pm.me>
2022-03-18 17:03:35 -0700

    Adds wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Snazzy",
+	font = wezterm.font("Fira Code", { weight = 450, stretch = "Normal", italic = false }),
+	font_size = 14,
+	window_background_opacity = 0.95,
+}
