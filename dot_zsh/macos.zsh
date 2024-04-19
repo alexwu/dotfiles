48c34f8e58727ed62e502a01d91ab9f78a5eb84b
Alex Wu <adwu@pm.me>
2024-04-18 18:22:49 -0700

    updates various aerospace and wezterm configs

--- dot_zsh/macos.zsh
+++ dot_zsh/macos.zsh
@@ -22,11 +22,14 @@ fi
 
 alias co="git checkout"
 
-if (( $+commands[mise] ))
-then
+# if (( $+commands[mise] ))
+# then
   eval "$(mise activate zsh)"
-fi
+# fi
 
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+source ~/.zsh/wezterm.sh
 
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export NEOVIDE_MULTIGRID=true
@@ -36,3 +39,35 @@ export HOMEBREW_NO_ENV_HINTS=true
 export OP_BIOMETRIC_UNLOCK_ENABLED=true
 path=(/Applications/Postgres.app/Contents/Versions/latest/bin $path)
 path=($HOME/.bin/nvim/bin $path)
+export SSH_AUTH_SOCK=""~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
+
+if (( $+commands[wezterm] ))
+then
+  # This script will open a new terminal for the selected project.
+  # Terminal used: wezterm
+
+# hacky way to switch workspace via the wezterm cli
+# source: https://github.com/wez/wezterm/issues/2979#issuecomment-1447519267
+# watch discussion: https://github.com/wez/wezterm/discussions/3534
+# watch issue: https://github.com/wez/wezterm/issues/3542
+wezterm-switch-workspace() {
+  args=$(jq -n --arg workspace "$1" --arg cwd "$2" '{"workspace":$workspace,"cwd":$cwd}' | base64)
+  printf "\033]1337;SetUserVar=%s=%s\007" switch-workspace $args
+}
+
+# List of project roots, all are assumed to be below $HOME
+# project_roots=(
+#   "$HOME/Source"
+#   "$XDG_CONFIG_HOME"
+# )
+
+# 1. Get all project folders without trailing slash
+# project_folders=$(fd --min-depth 1 --max-depth 1 -t directory . ${project_roots[@]} | xargs realpath)
+
+# 2. Select a project using fzf (all paths are relative to $HOME)
+# selected_folder=$(printf '%s\n' "${project_folders[@]}" | sed "s|$HOME/||" | fzf)
+
+# 3. Switch to the workspace by communicating with wezterm
+# wezterm-switch-workspace $selected_folder $HOME/$selected_folder
+fi
